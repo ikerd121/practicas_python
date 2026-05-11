c24357726bc9f57b0aad7c9d5ed7aaacde818113
ikerd121 <ikercaramelli@gmail.com>
2026-05-11 11:04:08 +0200

    .

---
+++ actividad 14.py
@@ -0,0 +1,8 @@
+equipos = []
+
+for contador in range(5):
+    nombre_equipo = input("Introduce el nombre del equipo: ")
+    equipos.append(nombre_equipo)
+
+print("Equipos introducidos:")
+print(equipos)
